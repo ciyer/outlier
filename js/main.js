c4630c5c4da83c9704ffa43ef9c6fc63da5ba32d
Chandrasekhar Ramakrishnan <ciyer@illposed.com>
2015-10-19 12:31:07 +0200

    Switched to production configuratin for require

--- js/main.js
+++ js/main.js
@@ -8,28 +8,35 @@
 
 
 // Development config
+// require.config({
+//   "urlArgs": "bust=" +  (new Date()).getTime(),
+//   "paths": {
+//     "babel": "babel/babel-5.8.22.min",
+//     "es6": "babel/es6",
+//     "backbone": "backbone/backbone",
+//     "d3": "d3/d3",
+//     "enquire": "enquire/enquire",
+//     "jquery": "jquery/jquery",
+//     "react": "react/react",
+//     "react-dom": "react/react-dom",
+//     "underscore": "underscore/underscore"
+//   }
+// });
+
+// Production config
 require.config({
-  "urlArgs": "bust=" +  (new Date()).getTime(),
   "paths": {
     "babel": "babel/babel-5.8.22.min",
     "es6": "babel/es6",
-    "backbone": "backbone/backbone",
-    "d3": "d3/d3",
+    "backbone": "https://cdnjs.cloudflare.com/ajax/libs/backbone.js/1.2.3/backbone-min",
+    "d3": "https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.6/d3.min",
     "enquire": "enquire/enquire",
-    "jquery": "jquery/jquery",
-    "react": "react/react",
-    "react-dom": "react/react-dom",
-    "underscore": "underscore/underscore"
+    "jquery": "https://code.jquery.com/jquery-1.11.3.min",
+    "react": "https://cdnjs.cloudflare.com/ajax/libs/react/0.14.0/react.min",
+    "react-dom": "https://cdnjs.cloudflare.com/ajax/libs/react/0.14.0/react-dom.min",
+    "underscore": "https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min"
   }
 });
-
-// Production config
-// require.config({
-//   "paths": {
-//     "jquery": "https://cdnjs.cloudflare.com/ajax/libs/jquery/1.11.3/jquery.min",
-//     "d3": "https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.6/d3.min"
-//   }
-// });
 
 requirejs(["app/app"], function(app) {
   $(document).ready(app.enter)
