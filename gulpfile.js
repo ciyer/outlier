8ec46bf305f46bdcca3ee1a871c1ef83deb94715
Chandrasekhar Ramakrishnan <ciyer@illposed.com>
2017-02-20 11:27:01 +0100

    Restructured code on file system for a gulp project.

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+var gulp = require('gulp');
+var preprocess = require('gulp-preprocess');
+var babel = require('gulp-babel');
+var watch = require('gulp-watch');
+var batch = require('gulp-batch');
+
+gulp.task('build', function() {
+  gulp.src(['./lib/**/*.*'])
+    .pipe(gulp.dest('./dist'));
+
+  gulp.src(['./src/**/*.html', './src/**/*.js'])
+    .pipe(preprocess())
+    .pipe(gulp.dest('./dist'));
+
+  gulp.src('./src/**/*.es6')
+    .pipe(babel({
+      presets: ['es2015', 'react'],
+      //plugins: ["transform-es2015-modules-amd", "add-module-exports"]
+      plugins: ["add-module-exports", "transform-es2015-modules-amd"]
+    }))
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('watch', function () {
+    watch(['./src/**/*.*'],
+      batch(function (events, done) {
+        gulp.start('build', done);
+      })
+    );
+});
+
+gulp.task('default', function() {
+  gulp.start('build');
+  gulp.start('watch');
+});
