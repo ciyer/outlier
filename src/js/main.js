9c5f04bd5aefde1f86497797005c002c3f37b0cb
Chandrasekhar Ramakrishnan <ciyer@illposed.com>
2017-02-20 13:30:19 +0100

    Switch to React 15.

--- src/js/main.js
+++ src/js/main.js
@@ -11,8 +11,6 @@
 // require.config({
 //   "urlArgs": "bust=" +  (new Date()).getTime(),
 //   "paths": {
-//     "babel": "babel/babel-5.8.22.min",
-//     "es6": "babel/es6",
 //     "backbone": "backbone/backbone",
 //     "d3": "d3/d3",
 //     "enquire": "enquire/enquire",
@@ -26,14 +24,12 @@
 // Production config
 require.config({
   "paths": {
-    "babel": "babel/babel-5.8.22.min",
-    "es6": "babel/es6",
     "backbone": "https://cdnjs.cloudflare.com/ajax/libs/backbone.js/1.2.3/backbone-min",
     "d3": "https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.6/d3.min",
     "enquire": "enquire/enquire",
     "jquery": "https://code.jquery.com/jquery-1.11.3.min",
-    "react": "https://cdnjs.cloudflare.com/ajax/libs/react/0.14.0/react.min",
-    "react-dom": "https://cdnjs.cloudflare.com/ajax/libs/react/0.14.0/react-dom.min",
+    "react": "https://unpkg.com/react@15/dist/react.min",
+    "react-dom": "https://unpkg.com/react-dom@15/dist/react-dom.min",
     "underscore": "https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min"
   }
 });
