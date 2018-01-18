e9bcb71dccac1d4135009ce99a774141863812ce
Chandrasekhar Ramakrishnan <cramakri@ethz.ch>
2018-01-18 22:36:35 +0100

    Bust cache for local files in production.

--- src/js/main.js
+++ src/js/main.js
@@ -21,8 +21,9 @@
 //   }
 // });
 
-// Production config
+// Production, urlbust
 require.config({
+  "urlArgs": "bust=" +  (new Date()).getTime(),
   "paths": {
     "backbone": "https://cdnjs.cloudflare.com/ajax/libs/backbone.js/1.2.3/backbone-min",
     "d3": "https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.6/d3.min",
@@ -33,6 +34,19 @@ require.config({
     "underscore": "https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min"
   }
 });
+
+// Production config
+// require.config({
+//   "paths": {
+//     "backbone": "https://cdnjs.cloudflare.com/ajax/libs/backbone.js/1.2.3/backbone-min",
+//     "d3": "https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.6/d3.min",
+//     "enquire": "enquire/enquire",
+//     "jquery": "https://code.jquery.com/jquery-1.11.3.min",
+//     "react": "https://unpkg.com/react@15/dist/react.min",
+//     "react-dom": "https://unpkg.com/react-dom@15/dist/react-dom.min",
+//     "underscore": "https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min"
+//   }
+// });
 
 requirejs(["app/app"], function(app) {
   $(document).ready(app.enter)
