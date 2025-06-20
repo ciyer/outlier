4d86a098af2f56e8f3b6a93fd80ff0ab1654d63d
Chandrasekhar Ramakrishnan <ciyer+air@illposed.com>
2025-06-21 00:13:16 +0200

    wip: converting to react-router 7

--- src/component/index.js
+++
@@ -1,9 +0,0 @@
-import About from "./About.container";
-import Archive from "./archive";
-import Article from "./article";
-import MarkdownPage from "./markdown";
-import ProductPage from "./product";
-import { AppFooter, AppNavBar } from "./nav";
-
-export { About, Archive, Article, MarkdownPage, ProductPage };
-export { AppFooter, AppNavBar };
