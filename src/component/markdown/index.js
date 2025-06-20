3cdcc819fae64b6cb61585be398e7a4665fe18d7
Chandrasekhar Ramakrishnan <ciyer+air@illposed.com>
2025-06-21 00:13:16 +0200

    wip: converting to react-router 7

--- src/component/markdown/index.js
+++
@@ -1,5 +0,0 @@
-import MarkdownPage from './MarkdownPage.container';
-import { MarkdownPageWrapper, mapStateToProps, mapDispatchToProps } from './MarkdownPage.container';
-
-export default  MarkdownPage; 
-export { MarkdownPageWrapper, mapStateToProps, mapDispatchToProps };
--- src/data/index.js
+++
@@ -1,6 +0,0 @@
-import Data from './Data';
-import { ReleaseSummary, ReleaseBaselineStats, ReleaseColorSummary, ReleaseFabricSummary } from './ReleaseSummary';
-import { releasesForProduct, outlierProductUrls } from './Utils'
-
-
-export { Data, ReleaseSummary, ReleaseBaselineStats, ReleaseColorSummary, ReleaseFabricSummary, releasesForProduct, outlierProductUrls }
