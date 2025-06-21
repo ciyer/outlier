cdb8ab0c0ce2897e4f3b49d2bc6f13e668206c28
Chandrasekhar Ramakrishnan <ciyer+air@illposed.com>
2025-06-21 07:28:05 +0200

    revive collections

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,21 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { createBrowserRouter, RouterProvider } from "react-router";
+import { Outlet, RouterProvider, createBrowserRouter } from "react-router";
 
 import { store } from "./app/store";
 
-import Root from "./features/root";
-import About from "./features/about.mdx";
+import Root from "./app/root";
+import About from "./routes/about.mdx";
+import {
+  Collections,
+  Ideas1,
+  Ideas2,
+  Ideas3,
+  Ideas4,
+  Ideas5,
+  Ideas6,
+  Ideas7,
+} from "./routes/collections";
 
 import "./styles/index.scss";
 import "./index.css";
@@ -23,6 +32,20 @@ const router = createBrowserRouter([
     children: [
       //      { index: true, Component: Home },
       { path: "about", Component: About },
+      {
+        path: "collections",
+        Component: Outlet,
+        children: [
+          { index: true, Component: Collections },
+          { path: "ideas/ideas-1", Component: Ideas1 },
+          { path: "ideas/ideas-2", Component: Ideas2 },
+          { path: "ideas/ideas-3", Component: Ideas3 },
+          { path: "ideas/ideas-4", Component: Ideas4 },
+          { path: "ideas/ideas-5", Component: Ideas5 },
+          { path: "ideas/ideas-6", Component: Ideas6 },
+          { path: "ideas/ideas-7", Component: Ideas7 },
+        ],
+      },
     ],
   },
 ]);
