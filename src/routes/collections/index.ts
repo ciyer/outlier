05ae230da47f26bf72a2b456da445e64e91d308b
Chandrasekhar Ramakrishnan <ciyer+air@illposed.com>
2025-06-21 07:28:05 +0200

    revive collections

---
+++ src/routes/collections/index.ts
@@ -0,0 +1,10 @@
+import Collections from "./Collections";
+import Ideas1 from "./Ideas1";
+import Ideas2 from "./Ideas2";
+import Ideas3 from "./Ideas3";
+import Ideas4 from "./Ideas4";
+import Ideas5 from "./Ideas5";
+import Ideas6 from "./Ideas6";
+import Ideas7 from "./Ideas7";
+
+export { Collections, Ideas1, Ideas2, Ideas3, Ideas4, Ideas5, Ideas6, Ideas7 };
