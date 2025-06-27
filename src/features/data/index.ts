2313cd9c138a60cba0131ebca6ca3a42967ba0c1
Chandrasekhar Ramakrishnan <ciyer+air@illposed.com>
2025-06-27 18:14:57 +0200

    revive data

---
+++ src/features/data/index.ts
@@ -0,0 +1,6 @@
+import Data from './Data';
+import { ReleaseSummary, ReleaseBaselineStats, ReleaseColorSummary, ReleaseFabricSummary } from './ReleaseSummary';
+import { releasesForProduct, outlierProductUrls } from './Utils'
+
+
+export { Data, ReleaseSummary, ReleaseBaselineStats, ReleaseColorSummary, ReleaseFabricSummary, releasesForProduct, outlierProductUrls }
