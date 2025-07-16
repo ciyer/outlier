342778756c6a53a11189a80b6e049082e071d1ee
Chandrasekhar Ramakrishnan <ciyer+air@illposed.com>
2025-07-16 14:59:17 +0200

    show footer correctly

--- src/app/root.tsx
+++ src/app/root.tsx
@@ -6,7 +6,7 @@ import "./App.css";
 
 export default function Root() {
   return (
-    <div>
+    <>
       <AppNavBar />
       <div className="mt-3">
         <main role="main" className="container-fluid">
@@ -14,6 +14,6 @@ export default function Root() {
         </main>
       </div>
       <AppFooter />
-    </div>
+    </>
   );
 }
