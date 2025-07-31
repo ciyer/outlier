ca25831a35813e41160a6ffcb00ebcbd26aa9fbc
Chandrasekhar Ramakrishnan <ciyer+air@illposed.com>
2025-07-31 16:53:24 -0400

    revive images

---
+++ src/features/archive/ArchiveWithImages.tsx
@@ -0,0 +1,78 @@
+import { Link, generatePath } from "react-router";
+import { Col, Row, Table } from "reactstrap";
+
+import { type DataRow } from "./Data";
+import { PATHS } from "../../routes/route-paths";
+import { groupedByYearQuarter, urlStringForProductName } from "../utils";
+
+type ArchiveTextGroupRowProps = {
+  entry: DataRow;
+};
+function ReleaseImageTitled({ entry }: ArchiveTextGroupRowProps) {
+  const src = entry["Image"];
+  return (
+    <div
+      className="p-2"
+      style={{
+        width: "calc(15%)",
+        height: "calc(15%)",
+        minWidth: "100px",
+        minHeight: "100px",
+      }}
+    >
+      <Link
+        to={`${generatePath(PATHS.product, {
+          productId: urlStringForProductName(entry.Product),
+        })}`}
+      >
+        <p key={`${entry.Product}-${entry.Release}-title`}>{entry.Product}</p>
+        <img
+          key={`${entry.Product}-${entry.Release}-image`}
+          src={src}
+          alt={entry.Product}
+          style={{ width: "100%", height: "100%" }}
+        />
+      </Link>
+    </div>
+  );
+}
+
+type ReleaseImagesTitledProps = {
+  entries: DataRow[];
+};
+
+function ReleaseImagesTitled({ entries }: ReleaseImagesTitledProps) {
+  const images = entries.map((r, i) => (
+    <ReleaseImageTitled key={i} entry={r} />
+  ));
+  return <div className="d-flex flex-wrap">{images}</div>;
+}
+
+type ArchiveTextGroupRowsProps = {
+  group: { name: string; entries: DataRow[] };
+  showTitle: boolean;
+};
+
+function ArchiveImageGroup({ group, showTitle }: ArchiveTextGroupRowsProps) {
+  const title = showTitle ? <h3>{group.name}</h3> : <span></span>;
+  return (
+    <Row>
+      <Col>
+        {title}
+        <ReleaseImagesTitled entries={group.entries} />
+      </Col>
+    </Row>
+  );
+}
+
+interface ArchiveWithTextProps {
+  data: DataRow[];
+}
+
+export default function ArchiveWithImages({ data }: ArchiveWithTextProps) {
+  const groups = groupedByYearQuarter(data);
+  const children = groups.map((g) => (
+    <ArchiveImageGroup key={g.name} group={g} showTitle={groups.length > 1} />
+  ));
+  return children;
+}
